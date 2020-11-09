dfb987abd3dad5e056f6ea65efdcaa2acdc1860c
gksgks0726 <gksgks0726@naver.com>
2020-11-10 00:31:16 +0900

    문제풀이

--- jol ope7.c
+++ jol ope7.c
@@ -4,9 +4,10 @@ int main()
 {   
     int a = 0, b = 0;
     scanf("%d %d", &a, &b);
-
-    printf("%d\n", a && b);
-    printf("%d", a || b);
+    
+    printf("%d %d\n", ++a, b--);
+    printf("%d %d", a, b);
+    
     return 0;
 
 }
