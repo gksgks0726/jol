e959c8fe2042f4bf7423dcc970cdf934ca49f5a2
gksgks0726 <gksgks0726@naver.com>
2020-11-18 23:23:27 +0900

    정올 문제풀이

---
+++ jol for4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+   int h, m;
+   scanf("%d %d", &h, &m);
+   if(m < 45){
+       
+       m = m + 60;
+       h--;
+       
+       if (h < 0)
+       h = 23;
+       printf("%d %d", h, m - 45);
+   }
+    else if(m > 45)
+        printf("%d %d", h, m - 45);
+    
+
+    return 0;
+
+}
